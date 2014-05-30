6054a5798b33cb5a037e2d46ef5c38b4c3211ca6
E. Kevin Hall <hall@absinthe.org>
2014-05-30 17:17:59 -0400

    Add operations

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 (defn greet
   [req]
@@ -16,9 +17,38 @@
    :body "Goodbye cruel world!"
    :headers {}})
 
+(defn about
+  [req]
+  {:status 200
+   :body "This is me, about me"
+   :headers {}})
+
+(defn yo-name
+  [req]
+  (let [name (get-in req [:route-params :name])]
+    {:status 200
+     :body (str "Yo! " name "!+++" handle-dump)}))
+
+(def ops
+  {"+" + "-" - "*" * ":" /})
+
+(defn calc
+  [req]
+  (let [a (Integer. (get-in req [:route-params :a]))
+        op (get-in req [:route-params :op])
+        f (get ops op)
+        b (Integer. (get-in req [:route-params :b]))]
+    (if f
+      {:status 200
+       :body (str "Answer is: " (f a b))})))
+
 (defroutes app
   (GET "/" [] greet)
   (GET "/goodbye" [] goodbye)
+  (GET "/about" [] about)
+  (GET "/request" [] handle-dump)
+  (GET "/yo/:name" [] yo-name)
+  (GET "/calc/:a/:op/:b" [] calc)
   (not-found "Page not found!"))
 
 (defn -main [port]
